0d9978e8496ea1389a6a03b78daa50c4e3ba19ea
Ondrej Brablc <ondrej@brablc.com>
2024-06-10 12:51:27 +0200

    Fix for older python (namely 3.8)

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+set -e
+
 SCRIPT_PATH=$(readlink -f $0)
 SCRIPT_DIR=${SCRIPT_PATH%/*}
 cd "$SCRIPT_DIR"
 
-[ -d .venv ] || python3 -m venv .venv
-source .venv/bin/activate
-pip install -r requirements.txt
+VENV_DIR="$SCRIPT_DIR/.venv"
+
+[ -d .venv ] || python3 -m venv "$VENV_DIR"
+source "$VENV_DIR/bin/activate"
+"$VENV_DIR/bin/pip" install -r requirements.txt
 ln -sv "$SCRIPT_DIR/lazysar" /usr/local/bin/
